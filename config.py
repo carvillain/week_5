7288185da7cccfbc0ddbe7a6f0a72525a5de601c
carvillain <owen.carvill@gmail.com>
2021-08-31 20:24:19 -0500

    Day 2 homework

--- config.py
+++ config.py
@@ -1,16 +1,19 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Give access to the project on ANY OS
-# Allow outside files/folders to be added to the project from the 
+# Give access to the project in ANY OS we find ourselves in
+# Allow outside files/folders to be added to the project from the
 # base directory.
 
 class Config():
     """
-        Set config variable for the flask app, if not already created.
+        Set config variables for the flask app.
+        Using Environment variables where available otherwise
+        create the config variables if not done already.
     """
 
-    SECRET_KEY = os.environ.get('SECRETKEY') or 'You will never guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off Update Messages
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:99013Pog@127.0.0.1:5432/car-collection'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off Update Messages from the sqlalchemy
